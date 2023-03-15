88f6192f9188809f5866f524a8b90a20f79322bf
DoChuan13 <chuando.9213@gmail.com>
2023-03-15 15:59:03 +0700

    FizzBuzz

--- src/FizzBuzz/FizzBuzzTranslate.java
+++ src/FizzBuzz/FizzBuzzTranslate.java
@@ -4,9 +4,6 @@ public class FizzBuzzTranslate {
     public static String FizzBuzz(int number) {
         boolean isFizz = number % 3 == 0;
         boolean isBuzz = number % 5 == 0;
-        String numberConvert = String.valueOf(number);
-        boolean isContains3 = numberConvert.contains("3");
-        boolean isContains5 = numberConvert.contains("5");
         if ((isFizz && isBuzz)) {
             return "FizzBuzz";
         } else if (isFizz) {
